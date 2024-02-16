926b2ebec01ee1fa8413466fca60ca531ba5499a
Hayrunnisa Bulut <hayrunnisa.bulut99@gmail.com>
2024-02-16 23:20:42 +0300

    DEvfolio site oluşturma
    DEvfolio site oluşturma

---
+++ DevFolio2/Controllers/DefaultController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DevFolio2.Models;
+
+namespace DevFolio2.Controllers
+{
+    public class DefaultController : Controller
+    {
+        DbDevFolioEntities db = new DbDevFolioEntities();
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialFeature()
+        {
+            var values = db.TblFeature.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialProfile()
+        {
+            var values = db.TblProfile.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialSkill()
+        {
+            var values = db.TblSkill.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialAbout()
+        {
+            var values = db.TblAbout.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialService()
+        {
+            var values = db.TblService.ToList();
+            return PartialView(values);
+        }
+    }
+}
