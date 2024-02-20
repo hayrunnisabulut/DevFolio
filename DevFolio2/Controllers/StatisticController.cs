ee01229268188405a30ef8aec4ba8f29b2e0da95
Hayrunnisa Bulut <hayrunnisa.bulut99@gmail.com>
2024-02-20 23:52:42 +0300

    statistics

--- DevFolio2/Controllers/StatisticController.cs
+++ DevFolio2/Controllers/StatisticController.cs
@@ -15,9 +15,9 @@ namespace DevFolio2.Controllers
             ViewBag.categoryCount = db.TblCategory.Count();
             ViewBag.projectCount = db.TblProject.Count();
             ViewBag.skillCount = db.TblSkill.Count();
-            ViewBag.skillAvgValue = db.TblSkill.Average(x => x.SkillValue);
+            ViewBag.skillAvgValue = Math.Round(Convert.ToDecimal(db.TblSkill.Average(x => x.SkillValue)),2).ToString();
             ViewBag.lastSkillTitle = db.GetLastSkillTitle().FirstOrDefault();
-            ViewBag.coreCategoryProjectCount = db.TblProject.Where(x => x.ProjectCategory == 1).Count();
+            ViewBag.netCategoryProjectCount = db.TblProject.Where(x => x.ProjectCategory == 8).Count();
             ViewBag.eniyihizmet = db.GetBestSkill().FirstOrDefault();
             ViewBag.hakkimdakelimesayisi = db.TblAbout.Where(x => x.AboutID == 1).ToList()[0].Description.Split().Count();
             ViewBag.sosyalMedyaSayisi = db.TblSocialMedia.Where(x => x.Status == true).Count();
