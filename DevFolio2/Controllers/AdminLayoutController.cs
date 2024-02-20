72b94cb491476cd09e830635416fa4d36a6d52a1
Hayrunnisa Bulut <hayrunnisa.bulut99@gmail.com>
2024-02-21 01:06:37 +0300

    dashboard düzenlendi

--- DevFolio2/Controllers/AdminLayoutController.cs
+++ DevFolio2/Controllers/AdminLayoutController.cs
@@ -1,14 +1,16 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DevFolio2.Models;
 
 namespace DevFolio2.Controllers
 {
     public class AdminLayoutController : Controller
     {
-        // GET: AdminLayout
+        DbDevFolioEntities db = new DbDevFolioEntities();
         public ActionResult Index()
         {
             return View();
@@ -16,9 +18,12 @@ namespace DevFolio2.Controllers
 
         public PartialViewResult Dashboard()
         {
+            ViewBag.gelenKutusu = db.TblContact.Where(x => x.IsRead == false).Count().ToString();
+            DateTime dt = Convert.ToDateTime(db.TblProject.Max(x => x.CreateDate));
+            ViewBag.sonprojetarihi = dt.ToString("dd/MM/yyyy");
+            ViewBag.referans = db.TblTestimonial.Count().ToString();
             return PartialView();
         }
-
         public PartialViewResult HeadPartial()
         {
             return PartialView();
